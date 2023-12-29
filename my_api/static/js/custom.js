85158bbc997eee4f60c61913f855a1a364d2ad89
oerijogoo ronald <joerironald@gmail.com>
2023-12-29 17:36:04 +0000

    slidess

--- my_api/static/js/custom.js
+++ my_api/static/js/custom.js
@@ -1,45 +1,44 @@
+(function ($) {
 
-  (function ($) {
-  
   "use strict";
 
-    // NAVBAR
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
+  // NAVBAR
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
-    $(function() {
-      $('.hero-slides').vegas({
-          slides: [
-              { src: 'images/slides/sincere-laugh-showing-picture-smartphone-casual-meeting-with-best-friends-restaurant-terrace.jpg' },
-              { src: 'images/happy-waitress-giving-coffee-customers-while-serving-them-coffee-shop.jpg' },
-              { src: 'images/young-female-barista-wear-face-mask-serving-take-away-hot-coffee-paper-cup-consumer-cafe.jpg' }
-          ],
-          timer: false,
-          animation: 'kenburns',
-      });
+  $(function () {
+    $('.hero-slides').vegas({
+      slides: [
+        { src: '{% static "images/slides/sincere-laugh-showing-picture-smartphone-casual-meeting-with-best-friends-restaurant-terrace.jpg" %}' },
+        { src: '{% static "images/happy-waitress-giving-coffee-customers-while-serving-them-coffee-shop.jpg" %}' },
+        { src: '{% static "images/young-female-barista-wear-face-mask-serving-take-away-hot-coffee-paper-cup-consumer-cafe.jpg" %}' }
+      ],
+      timer: false,
+      animation: 'kenburns',
     });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height() + 60;
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
+  });
+
+  // CUSTOM LINK
+  $('.smoothscroll').click(function () {
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 60;
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $('body,html').animate({
         scrollTop: totalScroll
-        }, 300);
-      }
-    });
-  
-  })(window.jQuery);
+      }, 300);
+    }
+  });
+
+})(window.jQuery);
 
 
