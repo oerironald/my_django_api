db5d51272fe5f2e081cd2360f4aaa115690c05a9
oerijogoo ronald <joerironald@gmail.com>
2024-01-02 12:59:24 +0000

    urls

--- canada/urls.py
+++ canada/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index1),
+    path('', views.index1,  name='index1'),
+    path('canada2/', views.index2,  name='index2'),
+    path('canada3/', views.index3,  name='index3'),
     
 ]
--- canada/views.py
+++ canada/views.py
@@ -6,4 +6,18 @@ def index1(request):
     
     return render(request,'canada/index.html')
 
+
+def index2(request):
+    
+    return render(request,'canada/index2.html') 
+
+
+
+def index3(request):
+    
+    return render(request,'canada/index3.html')
+
+
+
+
 # Create your views here.
