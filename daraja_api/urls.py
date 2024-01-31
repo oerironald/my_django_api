966a8f731134fd92005656e38754bf5d53deee6a
oerijogoo <51825282+oerijogoo@users.noreply.github.com>
2024-01-31 11:15:16 +0000

    descr

--- daraja_api/urls.py
+++ daraja_api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get),
-    path('push', views.make_payment),
-    path('', views.get),
-    path('index/<str:phone_number>/<int:amount>/', views.index, name='index'),
-    path('pay', views.stk_push_callback, name='mpesa_stk_push_callback'),
+    # path('', views.get),
+    # path('push', views.make_payment),
+    # path('', views.get),
+    # path('index/<str:phone_number>/<int:amount>/', views.index, name='index'),
+    # path('pay', views.stk_push_callback, name='mpesa_stk_push_callback'),
     path('payment', views.payment_form, name='payment'),
     path('paymentpro', views.process_payment, name='paymentpro'),
 ]
--- daraja_api/views.py
+++ daraja_api/views.py
@@ -13,89 +13,89 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-@api_view(['GET'])
-def get(request):
+# @api_view(['GET'])
+# def get(request):
 
-        access_token = generate_access_token()
-        formated_time = timestamp_conversion()
-        decoded_password = generate_password(formated_time)
+#         access_token = generate_access_token()
+#         formated_time = timestamp_conversion()
+#         decoded_password = generate_password(formated_time)
 
-        return Response({"access_token": access_token, "password": decoded_password})
+#         return Response({"access_token": access_token, "password": decoded_password})
 
 
 
-@api_view(['POST'])
-def make_payment(request, *args, **kwargs):
-        requestData = request.data
-        amount = requestData["amount"]
-        phone = requestData["phone_number"]
+# @api_view(['POST'])
+# def make_payment(request, *args, **kwargs):
+#         requestData = request.data
+#         amount = requestData["amount"]
+#         phone = requestData["phone_number"]
 
-        paymentResponseData =  make_mpesa_payment_request(amount= amount, phone= phone)
+#         paymentResponseData =  make_mpesa_payment_request(amount= amount, phone= phone)
 
-        return Response(paymentResponseData)
+        # return Response(paymentResponseData)
 
-def make_mpesa_payment_request(amount: str, phone: str) -> dict:
-    access_token = generate_access_token()
-    formated_time = timestamp_conversion()
-    decoded_password = generate_password(formated_time)
+# def make_mpesa_payment_request(amount: str, phone: str) -> dict:
+#     access_token = generate_access_token()
+#     formated_time = timestamp_conversion()
+#     decoded_password = generate_password(formated_time)
 
-    headers = {"Authorization": "Bearer %s" % access_token}
+#     headers = {"Authorization": "Bearer %s" % access_token}
 
-    request = {
-        "BusinessShortCode": settings.BUSINESS_SHORT_CODE,
-        "Password": decoded_password,
-        "Timestamp": formated_time,
-        "TransactionType": settings.TRANSACTION_TYPE,
-        "Amount": amount,
-        "PartyA": phone,
-        "PartyB": settings.BUSINESS_SHORT_CODE,
-        "PhoneNumber": phone,
-        "CallBackURL": settings.CALL_BACK_URL,
-        "AccountReference": settings.ACCOUNT_REFFERENCE,
-        "TransactionDesc": settings.TRANSACTION_DESCRIPTION
-    }
+#     request = {
+#         "BusinessShortCode": settings.BUSINESS_SHORT_CODE,
+#         "Password": decoded_password,
+#         "Timestamp": formated_time,
+#         "TransactionType": settings.TRANSACTION_TYPE,
+#         "Amount": amount,
+#         "PartyA": phone,
+#         "PartyB": settings.BUSINESS_SHORT_CODE,
+#         "PhoneNumber": phone,
+#         "CallBackURL": settings.CALL_BACK_URL,
+#         "AccountReference": settings.ACCOUNT_REFFERENCE,
+#         "TransactionDesc": settings.TRANSACTION_DESCRIPTION
+#     }
 
-    response = requests.post(settings.API_RESOURCE_URL, json=request, headers=headers)
+#     response = requests.post(settings.API_RESOURCE_URL, json=request, headers=headers)
 
-    try:
-        obbstr = response.json()
-    except json.JSONDecodeError:
-        # Handle JSON parsing error if necessary
-        obbstr = {}
+#     try:
+#         obbstr = response.json()
+#     except json.JSONDecodeError:
+#         # Handle JSON parsing error if necessary
+#         obbstr = {}
 
-    merchant_request_id = obbstr.get('MerchantRequestID')
-    checkout_request_id = obbstr.get('CheckoutRequestID')
-    response_description = obbstr.get('ResponseDescription')
-    response_code = obbstr.get('ResponseCode')
+#     merchant_request_id = obbstr.get('MerchantRequestID')
+#     checkout_request_id = obbstr.get('CheckoutRequestID')
+#     response_description = obbstr.get('ResponseDescription')
+#     response_code = obbstr.get('Reget_access_token()sponseCode')
 
-    data = {
-        "merchant_request_id": merchant_request_id,
-        "checkout_request_id": checkout_request_id,
-        "response_description": response_description,
-        "response_code": response_code
-    }
+#     data = {
+#         "merchant_request_id": merchant_request_id,
+#         "checkout_request_id": checkout_request_id,
+#         "response_description": response_description,
+#         "response_code": response_code
+#     }
 
-    return data
+#     return data
 
         
 
 
-def index(request, phone_number, amount):
-    cl = MpesaClient()
-    # Use a Safaricom phone number that you have access to, for you to be able to view the prompt.
+# def index(request, phone_number, amount):
+#     cl = MpesaClient()
+#     # Use a Safaricom phone number that you have access to, for you to be able to view the prompt.
     
-    account_reference = 'reference'
-    transaction_desc = 'Description'
-    callback_url = 'https://ominous-system-wg6x6w4vjjpfgr59-8000.app.github.de'
-    response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
-    return HttpResponse(response)
+#     account_reference = 'reference'
+#     transaction_desc = 'Description'
+#     callback_url = 'https://ominous-system-wg6x6w4vjjpfgr59-8000.app.github.de'
+#     response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
+#     return HttpResponse(response)
 
 
-def stk_push_callback(request):
-        data = request.body
+# def stk_push_callback(request):
+#         data = request.body
         
-        #return HttpResponse("STK Push in Django👋")
-        return HttpResponse(data)
+#         #return HttpResponse("STK Push in Django👋")
+#         return HttpResponse(data)
 
 
 
@@ -109,10 +109,11 @@ def process_payment(request):
         amount = int(request.POST.get('amount'))  # Convert amount to an integer
 
         cl = MpesaClient()
-        account_reference = 'reference'
-        transaction_desc = 'Description'
-        callback_url = 'https://ominous-system-wg6x6w4vjjpfgr59-8000.app.github.de'
+        account_reference = 'Jogoo Oeri'
+        transaction_desc = 'Payments'
+        callback_url = 'https://ominous-system-wg6x6w4vjjpfgr59-8000.app.github.dev'
         response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
         return HttpResponse(response)
 
         return HttpResponseBadRequest("Invalid request method")
+        
